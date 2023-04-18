d4e4333da49013f7dd2db800b8605a406b3c2c2f
callistonianembrace <87455417+callistonianembrace@users.noreply.github.com>
2023-04-18 09:31:24 +0530

    class_group_vdf: Fixes $PATH calculation for non-Windows

--- packages/class_group_vdf.0.0.4/files/build.sh
+++ packages/class_group_vdf.0.0.4/files/build.sh
@@ -1,4 +1,13 @@
 YARN_PKG_CONFIG_BIN_PATH=$1
 
-PATH_V=$(echo "$YARN_PKG_CONFIG_BIN_PATH;$PATH" | sed 's/;;/;/g')
-env PATH=$(cygpath -u "$PATH_V") dune build -p class_group_vdf
+case "$(uname -s)" in
+    CYGWIN*|MINGW32*|MSYS*)
+	PATH_V=$(echo "$YARN_PKG_CONFIG_BIN_PATH;$PATH" | sed 's/;;/;/g')
+	PATH_V=$(cygpath -u "$PATH_V")
+	;;
+    *)
+	PATH_V=$PATH
+	;;
+esac
+
+env PATH="$PATH_V" dune build -p class_group_vdf
